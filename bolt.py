3df5932a68f7f1b1ed3432900eed1dff97958a96
csekok <csekok@gamma.ttk.pte.hu>
2021-12-02 18:28:50 +0100

    Initial commit

---
+++ bolt.py
@@ -0,0 +1,63 @@
+
+
+
+class Bolt:
+    """
+    A vásárlásokat kezelő osztály. Az osztály egyetlen attribútuma a kosarak listája.
+    """
+
+    def __init__(self):
+        """
+        A bolt létrehozásakor beállítja az osztály attribútumait.
+        """
+        def Kosar:
+            Kosar(kosarak)
+            list[kosarak]
+
+    def feladat_1(self, filepath: str) -> None:
+        """
+        Beolvassa a fájlból a kosarak tartalmát.
+
+        :param filepath: A kosarak tartalmát tartalmazó fájl elérési útvonala.
+        """
+
+
+    def feladat_2(self) -> None:
+        """
+        Kiírja, hányan fizettek a pénztárnál.
+        """
+        F = 0
+        for i in range(len(dict)):
+            if(i = F):
+                F += 1
+        print("feladat_2:" f(F) "alkalommal fizettek")
+
+
+
+
+    def feladat_3(self) -> None:
+        """
+        Bekéri egy vásárlás sorszámát és kiírja:
+            - hány darab árucikk volt a kosárban,
+            - mely árucikkekből és milyen mennyiségben vásároltak,
+            - a vásárlás összegét.
+        """
+
+
+    def feladat_4(self) -> None:
+        """
+        Bekéri egy árucikk nevét és kiírja:
+            - melyik vásárlásnál vettek először a termékből
+            - melyik vásárlásnál vettek utoljára a termékből
+            - összesen hány alkalommal vásároltak a termékből
+        """
+        pass
+
+    def feladat_5(self, filepath: str) -> None:
+        """
+        Elmenti a megadott fájlba a vásárlásonként fizetendő összeget.
+        Beolvassa a fájlból a kosarak tartalmát.
+
+        :param filepath: A kosarak tartalmát tartalmazó fájl elérési útvonala.
+        """
+        pass
---
+++ kosar.py
@@ -0,0 +1,117 @@
+class Kosar:
+    """
+    Egyetlen vásárlás adatait kezelő osztály.
+
+    Az osztály attribútumai:
+        - a kosárban lévő árucikkek (név-mennyiség párok)
+        - a vásárlás összege
+    """
+
+    def __init__(self, termekek: dict[str, int]) -> None:
+        """
+        A kosár létrehozásakor beállítja az osztály attribútumait.
+        """
+        dict = [arunev]
+        arunev =[toll,colostok,HB_ceruza,szatyor,csavarkulcs,doboz,filctoll,kefe,ceruzaelem]
+        toll = 1000
+        colostok = 1000
+        HB_ceruza = 1000
+        szatyor = 1000
+        csavarkulcs = 1000
+        doboz = 1000
+        filctoll = 1000
+        kefe = 1000
+        ceruzaelem = 1000
+
+
+
+
+
+
+
+    def osszeg_lekerdezese(self) -> int:
+        """
+        A vásárlás összegének lekérdezése.
+
+        :return: A vásárlás összege Ft-ban.
+        """
+        osszeg = 0
+        for i in range(len(dict)):
+            while (dict[i] != F):
+                sum dict[i]
+                i += 1000
+                return osszeg
+
+
+
+
+    def termekek_lekerdezese(self) -> dict[str, int]:
+        """
+        Az árucikk-mennyiség párok lekérdezése.
+
+        :return: Az árucikkek nevei és mennyiségei.
+        """
+        for arunev in range(len(dict)):
+            while (dict[arunev] != F):
+                if (dict[arunev] = dict[arunev]):
+                    arunev += 1
+                    return arunev
+
+
+    def termekek_szamanak_lekerdezese(self) -> int:
+        """
+        A kosárban lévő termékek számának lekérdezése.
+
+        :return: Hány darab termék van a kosárban.
+        """
+        sum = 0
+        for i in range(len(dict)):
+            while (dict[i] != F):
+                sum += 1
+                return sum
+
+
+
+    def arucikk_mennyisegenek_lekerdezese(self, arucikk: str) -> int:
+        """
+        Egy árucikknek a kosárban megtalálható mennyiségének lekérdezése.
+
+        :param arucikk: A vizsgált árucikk neve.
+        :return: A vizsgált árucikk mennyisége a kosárban.
+        """
+        while(dict[arunev] != F):
+            toll = 0
+                if (toll = list[i]):
+                    toll += 1
+            colostok = 0
+                if (colostok = list[i]):
+                    colostok += 1
+            HB_ceruza = 0
+                if (HB_ceruza = list[i]):
+                    HB_ceruza += 1
+            szatyor = 0
+                if (szatyor = list[i]):
+                    szatyor += 1
+            csavarkulcs = 0
+                if (csavarkulcs = list[i]):
+                    csavarkulcs += 1
+            doboz = 0
+                if (doboz = list[i]):
+                    doboz += 1
+            filctoll = 0
+                if (filctoll = list[i]):
+                    filctoll += 1
+            kefe = 0
+                if (kefe = list[i]):
+                    kefe += 1
+            ceruzaelem = 0
+                if (ceruzaelem = list[i]):
+                    ceruzaelem += 1
+                return arunev
+
+    def kosar_tartalmanak_kiiratasa(self) -> None:
+        """
+        Kiírja a kosár tartalmát a konzolra.
+        """
+        print(termekek)
+
